d17bbf9e3d811e4e316af99adc58c9b355ee2049
GITA <jgitanjali592@gmail.com>
2024-03-18 17:23:31 +0530

    this code for nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,20 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
+    <div className='  h-screen w-screen' >
+
+        <nav className="w-[15%]   h-full bg-zinc-50 flex-col items-center pt-5">
+            <a  className = "py-2 px-5 border rounded border-blue-200  text-blue-300"href="/create">Add New Product</a>
+
+            <hr className=' my-3 w-[80%]'/>
+            <h1>Category</h1>
+            <ul>
+                <li>cat 1</li>
+            </ul>
+        </nav>
       
     </div>
-  )
-}
+  );
+};
 
 export default App
