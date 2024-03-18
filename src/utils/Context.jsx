dbed09bd77158e7a35e0c7b124613f7fb37347d6
GITA <jgitanjali592@gmail.com>
2024-03-18 20:31:08 +0530

    this code for axios

---
+++ src/utils/Context.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+const Context = () => {
+
+    const [products , setproducts] = useState(null);
+  return (
+    <div>
+      Context
+    </div>
+  )
+}
+
+export default Context
