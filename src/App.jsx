05b26bc16647daf208e82637423362b271344975
GITA <jgitanjali592@gmail.com>
2024-03-18 18:32:37 +0530

    this code for card design

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='  h-screen w-screen' >
+    <div className='  h-screen w-screen flex' >
 
         <nav className="w-[15%]   h-full bg-zinc-50 flex-col items-center pt-5">
-            <a  className = "py-2 px-5 border rounded border-blue-200  text-blue-300"href="/create">Add New Product</a>
+            <a  className = "py-2 px-5 border rounded border-blue-200  text-blue-300 ml-2"href="/create">Add New Product</a>
 
             <hr className=' my-3 w-[80%]'/>
 
@@ -20,7 +20,24 @@ const App = () => {
                 
             </ul>
         </nav>
+
       
+      <div className=' w-[85%] p-10  px-[5%] flex flex-wrap overflow-x-hidden overflow-y-auto '>
+
+
+        <div className=' mr-3 mb-3 card p-3 border shadow rounded w-[18%] h-[30vh] flex-col flex justify-center items-center'>
+
+        <div className=' hover:scale-110 mb-3 w-full h-[80%] bg-contain bg-no-repeat bg-center'
+        style={{backgroundImage:"URL(https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg)" , }}>
+
+        </div>
+        <h1 className='hover:text-blue-300'>Lorem ipsum dolor sit amet.</h1>
+      </div>
+
+
+
+      </div>
+
     </div>
   );
 };
