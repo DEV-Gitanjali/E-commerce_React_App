9343f830285974137f1091b06c7c55d4efd79f5e
GITA <jgitanjali592@gmail.com>
2024-03-18 17:42:21 +0530

    this code for nav list

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,16 @@ const App = () => {
             <a  className = "py-2 px-5 border rounded border-blue-200  text-blue-300"href="/create">Add New Product</a>
 
             <hr className=' my-3 w-[80%]'/>
-            <h1>Category</h1>
-            <ul>
-                <li>cat 1</li>
+
+
+            <h1 className='text-2xl mb-3 w-[100%] ml-3'>Category Filter</h1>
+            <ul className=' w-[80%] ml-3'>
+                <li className=' flex items-center mb-3'> <span className=' rounded-full mr-2 w-[15px] h-[15px] bg-blue-100'></span>cat 1</li>
+                
+                <li className=' flex items-center mb-3'> <span className=' rounded-full mr-2 w-[15px] h-[15px] bg-red-100'></span>cat 1</li>
+                
+                <li className=' flex items-center mb-3'> <span className=' rounded-full mr-2 w-[15px] h-[15px] bg-green-100'></span>cat 1</li>
+                
             </ul>
         </nav>
       
